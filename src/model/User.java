f0f259afcfef0c900e5fbf25a9a51ce588d6a1e6
unknown <41454237+ollinmagno@users.noreply.github.com>
2020-12-04 15:57:03 -0300

    classe modelo

---
+++ src/model/User.java
@@ -0,0 +1,40 @@
+package model;
+
+public class User {
+    private int id;
+    private String name;
+    private String login;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
